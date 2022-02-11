a6242cf873d397bb7847c63b3436a7709c03d7ef
wandalen <wandalen.me@gmail.com>
2022-02-11 12:17:49 +0200

    more practice

--- graphics/bevy_pong/src/main.rs
+++ graphics/bevy_pong/src/main.rs
@@ -1,15 +1,29 @@
 use bevy::prelude::*;
 
-//
+///
+/// Movable
+///
 
-#[derive(Component)]
+#[ derive( Component ) ]
 pub struct Movable
 {
   pub speed : f32,
   pub direction : Vec3,
 }
 
-//
+///
+/// Paddle
+///
+
+#[ derive( Component ) ]
+pub struct Paddle
+{
+  pub position : f32,
+}
+
+///
+/// Setup
+///
 
 fn setup( mut commands : Commands )
 {
@@ -18,16 +32,6 @@ fn setup( mut commands : Commands )
   let camera = OrthographicCameraBundle::new_2d();
 
   commands.spawn_bundle( camera );
-
-  // commands.spawn( Camera2DComponents::default() );
-  ball_spawn( &mut commands );
-
-}
-
-//
-
-fn ball_spawn( commands : &mut Commands )
-{
 
   let size = 25.0;
   let sprite = Sprite { custom_size : Some( Vec2::new( size, size ) ), .. Default::default() };
@@ -39,11 +43,30 @@ fn ball_spawn( commands : &mut Commands )
   .insert( Movable { speed : 1.0, direction : Vec3::new( 1.0, 1.0, 0.0 ).normalize() } )
   ;
 
+  let transform = Transform
+  {
+    translation : Vec3::new( 300.0, 0.0, 0.0 ),
+    .. Default::default()
+  };
+  let sprite = Sprite
+  {
+    custom_size : Some( Vec2::new( 10.0, 100.0 ) ),
+    .. Default::default()
+  };
+  commands.spawn_bundle( SpriteBundle
+  {
+    sprite,
+    transform,
+    .. Default::default()
+  })
+  .insert( Paddle { position : 0.0 } )
+  ;
+
 }
 
 //
 
-fn ball_move( mut query : Query<(&Movable, &mut Transform)> )
+pub fn ball_move( mut query : Query<( &Movable, &mut Transform )> )
 {
 
   let (movable, mut transform) = query.single_mut();
@@ -51,11 +74,46 @@ fn ball_move( mut query : Query<(&Movable, &mut Transform)> )
 
 }
 
-//
+///
+/// Print all resources
+///
+
+pub fn print_resources( archetypes : &Archetypes, components : &Components )
+{
+  let mut r : Vec<String> = archetypes
+  .resource()
+  .components()
+  .map(|id| components.get_info(id).unwrap())
+  // get_short_name removes the path information
+  // i.e. `bevy_audio::audio::Audio` -> `Audio`
+  // if you want to see the path info replace
+  // `TypeRegistration::get_short_name` with `String::from`
+  .map(|info| TypeRegistration::get_short_name(info.name()))
+  .collect();
+
+  // sort list alphebetically
+  r.sort();
+  r.iter().for_each(|name| println!("{}", name));
+}
+
+///
+/// The system iterates resize events and print them.
+///
+
+pub fn on_resize_system(mut resize_reader : EventReader<WindowResized>)
+{
+  for e in resize_reader.iter()
+  {
+    println!( "event {:?}", e );
+  }
+}
+
+///
+/// Main
+///
 
 fn main()
 {
-
   let mut app = App::new();
 
   app
@@ -63,6 +121,7 @@ fn main()
   // .add_plugin( bevy::diagnostic::LogDiagnosticsPlugin::default() )
   .add_plugin( bevy::diagnostic::FrameTimeDiagnosticsPlugin::default() )
   .add_startup_system( setup.system() )
+  .add_system( ball_move.system() )
   .add_system( ball_move.system() )
   .add_system( bevy::input::system::exit_on_esc_system )
   ;
